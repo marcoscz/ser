631c15d8e6ce995c206c32eec47fa11801433a3d
marcoscz <63687114+marcoscz@users.noreply.github.com>
2024-09-19 14:51:29 -0300

    go

--- ApiMangas/Entities/Entity.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace ApiMangas.Entities;
-
-public abstract class Entity
-{
-    public int Id { get; set; }
-}
--- ApiMangas/Entities/UploadResult.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ApiMangas.Entities;
-
-public class UploadResult
-{
-    public bool Uploaded { get; set; }
-    public string? FileName { get; set; }
-    public string? StoredFileName { get; set; }
-    public int ErrorCode { get; set; }
-}
